78930be56b21241febce8f477a4d201ab610725e
Harris Tzovanakis <me@drjova.com>
2022-04-12 08:41:40 +0200

    addition of Matomo

--- src/html.js
+++ src/html.js
@@ -19,6 +19,23 @@ export default function HTML({
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            var _paq = window._paq = window._paq || [];
+            /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+            _paq.push(['trackPageView']);
+            _paq.push(['enableLinkTracking']);
+            (function() {
+              var u="https://webanalytics.web.cern.ch/";
+              _paq.push(['setTrackerUrl', u+'matomo.php']);
+              _paq.push(['setSiteId', '40']);
+              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+              g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            })();
+                `,
+          }}
+        />
         {headComponents}
       </head>
       <body {...bodyAttributes}>
